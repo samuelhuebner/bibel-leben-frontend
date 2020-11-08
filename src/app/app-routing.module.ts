be34f6c8f0ebdca834079c19400fba3bef440003
Samuel Hübner <samu.huebner@gmail.com>
2020-11-08 19:47:34 +0100

    Adds logged-in guard for the auth route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { LoggedInGuard } from './guards/logged-in.guard';
 
 const routes: Routes = [
-  { path: 'auth', loadChildren: () => import('./modules/auth/auth-routing.module').then(m => m.AuthRoutingModule) },
-  { path: 'home', component: HomeComponent },
-  { path: '', component: HomeComponent }
+  {
+    path: 'auth',
+    loadChildren: () => import('./modules/auth/auth-routing.module')
+      .then(m => m.AuthRoutingModule),
+    canActivate: [LoggedInGuard]
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'home' }
 ];
 
 @NgModule({
