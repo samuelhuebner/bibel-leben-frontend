b648e37ac944ddb692cd0582e94522521eadcaf6
Samuel Hübner <samu.huebner@gmail.com>
2020-11-08 21:03:17 +0100

    Fixes Formbuilder

--- src/app/modules/auth/components/mail-register/mail-register.component.ts
+++ src/app/modules/auth/components/mail-register/mail-register.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 import { AuthService } from '../../../../services/auth.service';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-mail-register',
@@ -13,17 +13,26 @@ export class MailRegisterComponent implements OnInit {
   @Output() successEmitter = new EventEmitter<any>();
 
   public hidePassword = true;
+  public registerForm: FormGroup;
 
   constructor(
-    private authServ: AuthService,
-    private formB: FormBuilder
+    private authService: AuthService,
+    private formBuilder: FormBuilder
   ) { }
 
   ngOnInit(): void {
+    this.registerForm = new FormGroup({
+      firstname: new FormControl('', [Validators.required]),
+      lastname: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      confirmPassword: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      dataConsent: new FormControl('', Validators.required)
+    }, this.checkConfirmPassword );
   }
 
   // send entered data to register
-  onSubmit(registeringData): void {
+  onSubmit(): void {
     // this.authServ
     //   .register(this.f.email.value, this.f.pw.value)
     //   .then(() => this.successEmitter.emit(), (err) => { console.log(err); });
@@ -33,8 +42,8 @@ export class MailRegisterComponent implements OnInit {
   // custom validator
   // to check if password and confirm password match
   checkConfirmPassword(group: FormGroup): { notSame: boolean } {
-    const pw = group.controls.pw.value;
-    const confirmPw = group.controls.confirmPw.value;
+    const pw = group.controls.password.value;
+    const confirmPw = group.controls.confirmPassword.value;
 
     return pw === confirmPw ? null : { notSame: true }
   }
